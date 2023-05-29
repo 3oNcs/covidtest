3b071fcbf77a71798cf1a39fb97d92c9a59a717d
3oNcs <81162768+3oNcs@users.noreply.github.com>
2023-05-29 11:45:49 +0300

    Update main.py

--- covid-19 statistics/main.py
+++ covid-19 statistics/main.py
@@ -23,4 +23,4 @@ def index():
     return render_template("main.html",data=data)
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=8080)
+   app.run(host="0.0.0.0", port=5000, debug=True)
